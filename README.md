a9def166faeaa3bb80937d1c975d70bd69d9ae25
gt <geuryt@yahoo.com>
2023-03-08 14:17:04 -0500

    upload

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ChatForti
+
+This is a quick project that uses FastAPI to create an API endpoint that receives debug output from a Fortigate firewall. The firewall is configured with an automation stitch that triggers when high CPU usage is detected. The stitch runs troubleshooting commands and sends a HTTP post message with the debug output to the FastAPI endpoint. The FastAPI endpoint then forwards the debug output to the ChatGPT API for review.
+
+The response is saved to a Python object, which can then be utilized in various ways, such as forwarding to Slack, Teams, Email, Updating a ticket, and more.
+
+# Demo
+![alt text](https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNTY5MzU1M2MwYjQzZTM2NGM2ZmEzMTgzYWFiOGM2ZGEwYTY1ODY1MyZjdD1n/6GnmYGHkmC5PEuZngG/giphy.gif)
+## Tools
+- FastAPI
+- Fortigate Firewall
+- ChatGPT API
+
+
+## How to Use
+
+- Configure the Fortigate firewall to trigger the automation stitch when high CPU usage is detected
+- Set the HTTP post message to the FastAPI endpoint URL
+- The FastAPI endpoint will forward the debug output to the ChatGPT API for review
+![alt text](https://i.imgur.com/TKa2QjF.png)
+![alt text](https://i.imgur.com/MosYTMU.png)
+![alt text](https://i.imgur.com/QJfm0C4.png)
+## License
+This project is licensed under the MIT License.
---
+++ app/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, status, HTTPException, Body
+from rich import print
+import openai
+
+app = FastAPI()
+
+openai.api_key = "XXXXXXXXXXXXXXXXXXXXXXX"
+model_engine = "gpt-3.5-turbo"
+
+chatgpt_context = """
+As a Senior Network/Firewall Engineer, you are tasked with troubleshooting a network issue that is causing high CPU utilization. 
+You are given the output of debug commands and asked to review them to determine the potential cause of the issue. 
+Your task is to identify the underlying problem and provide recommendations on how to resolve it, do not provide any configuration commands. Pay
+close attention to the processes from the diagnose sys top command.
+This is a fortigate firewall.
+"""
+
+
+@app.post("/fortigate/troubleshoot/")
+async def troubleshoot_fortigate(body: str = Body(...)):
+    print(body)
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": f"{chatgpt_context}."},
+                {"role": "user", "content": f"{body}"},
+            ],
+        )
+        message = response.choices[0]["message"]
+        print(f"[bold yellow]:robot_face: ChatGPT Response[/bold yellow] :robot_face:")
+        print(message["content"])
+        print(f"[bold yellow]:robot_face: ChatGPT Response[/bold yellow] :robot_face:")
+        return status.HTTP_200_OK
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error",
+        )
